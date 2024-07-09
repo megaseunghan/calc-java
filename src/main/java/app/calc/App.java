54a032f5eed2268a99f04b27d025d91e5d6f5361
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-09 18:06:43 +0900

    :sparkles: 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정
    - 반복문을 사용한다.

--- src/main/java/app/calc/App.java
+++ src/main/java/app/calc/App.java
@@ -6,26 +6,35 @@ public class App {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        String input = "";
 
-        System.out.println("첫 번째 숫자를 입력하세요");
-        int firstNum = Integer.parseInt(sc.nextLine());
+        while (true) {
 
-        System.out.println("두 번째 숫자를 입력하세요");
-        int secondNum = Integer.parseInt(sc.nextLine());
+            if (input.equals("exit")) break;
 
-        System.out.println("사칙연산 기호를 입력하세요");
-        String operator = sc.nextLine();
+            Scanner sc = new Scanner(System.in);
 
-        int result = 0;
+            System.out.println("첫 번째 숫자를 입력하세요");
+            int firstNum = Integer.parseInt(sc.nextLine());
 
-        try {
-            result = calculate(firstNum, secondNum, operator);
-            System.out.printf("결과: %d", result);
-        } catch (Exception e) {
-            System.out.printf("에러 발생 : %s", e.getMessage());
+            System.out.println("두 번째 숫자를 입력하세요");
+            int secondNum = Integer.parseInt(sc.nextLine());
+
+            System.out.println("사칙연산 기호를 입력하세요");
+            String operator = sc.nextLine();
+
+            int result = 0;
+
+            try {
+                result = calculate(firstNum, secondNum, operator);
+                System.out.printf("결과: %d", result);
+            } catch (Exception e) {
+                System.out.printf("에러 발생 : %s", e.getMessage());
+            }
+
+            System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)");
+            input = sc.nextLine();
         }
-
     }
 
     private static int calculate(int first, int second, String operator) throws RuntimeException {
