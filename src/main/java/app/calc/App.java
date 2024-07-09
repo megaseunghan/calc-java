7e772acd7aacc3c1b3eaac445a6a20737474d722
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-09 17:45:16 +0900

    :sparkles: 1. Scanner를 사용하여 양의 정수(0 포함)를 전달 받을 수 있다.

--- src/main/java/app/calc/App.java
+++ src/main/java/app/calc/App.java
@@ -1,7 +1,19 @@
 package app.calc;
 
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요");
+        int firstNum = Integer.parseInt(sc.nextLine());
+
+        System.out.println("두 번째 숫자를 입력하세요");
+        int secondNum = Integer.parseInt(sc.nextLine());
+
     }
+
 }
