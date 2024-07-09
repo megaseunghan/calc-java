78118b9bca4817cd5255bc05e540e12b62bd6229
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-07-09 22:29:48 +0900

    :sparkles: 8. “inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력
    - foreach 활용

--- src/main/java/app/calc/App.java
+++ src/main/java/app/calc/App.java
@@ -18,9 +18,11 @@ public class App {
 
             if (input.equals("remove")) calculator.results.remove(0);
 
+            if (input.equals("inquiry")) calculator.results.forEach(elem -> System.out.print(elem + " "));
+
             Scanner sc = new Scanner(System.in);
 
-            System.out.println("첫 번째 숫자를 입력하세요");
+            System.out.println("\n첫 번째 숫자를 입력하세요");
             int firstNum = Integer.parseInt(sc.nextLine());
 
             System.out.println("두 번째 숫자를 입력하세요");
@@ -38,6 +40,7 @@ public class App {
             }
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             input = sc.nextLine();
         }
